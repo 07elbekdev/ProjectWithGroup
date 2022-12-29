cd4b8dd40d3ee1d6694538e779402539913e57c6
07elbekdev <masrapovelbek24@gmail.com>
2022-12-29 17:32:39 +0600

    I'm Elbek.

--- src/main/Service.java
+++ src/main/Service.java
@@ -1,4 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class Service {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        method();
+    }
+
+    public static void method() {
+
+    }
 }
