41223bee7f66e08a8db15e51e61cc8dc0593770d
daniel12142007 <danijelahatzanov@gmail.com>
2022-12-30 16:06:34 +0600

    I daniel

--- src/main/Service.java
+++ src/main/Service.java
@@ -6,18 +6,7 @@ import java.util.Scanner;
 
 public class Service {
     public static void main(String[] args) {
-        method();
-    }
 
-    public static void method() {
-        Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        String b = in.nextLine();
-        String result = a + b;
-        System.out.println(result);
-    }
-}
-        Methot();
     }
 
     public static void Methot() {
