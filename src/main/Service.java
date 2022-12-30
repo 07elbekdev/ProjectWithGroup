25b657dae922359405411b68671d5211f8ab34c9
07elbekdev <masrapovelbek24@gmail.com>
2022-12-30 23:00:14 +0600

    Hello world.

--- src/main/Service.java
+++ src/main/Service.java
@@ -1,25 +1,26 @@
 package main;
 
 import animal.BreedOfAnimals;
+import animal.Dog;
 
 import java.util.*;
 
 public class Service {
-    void search(){
+    void search() {
         Scanner a = new Scanner(System.in);
-        System.out.println("1) FInd by name : ");
+        System.out.println("1) Find by name : ");
         System.out.println("2) Find by color : ");
         System.out.println("3) Find by breed : ");
         System.out.println("4) Find by age : ");
         System.err.println("5) Exit : ");
-        String b = a.nextLine();
         int k = a.nextInt();
-        if (k==1){
-            System.out.println();
-        }
-        Map<String,BreedOfAnimals> array = new HashMap();
-        array.put("Mastiff", BreedOfAnimals.Mastiff);
-        array.put("Border", BreedOfAnimals.Border);
-        System.out.println(array);
+        if (k == 1) {
+            System.out.println("Find by name : ");
+            String some = a.next();
+            Map<Integer, String> array = new HashMap();
+            if (Objects.equals(some, BreedOfAnimals.Boxers)) {
+                System.out.println(BreedOfAnimals.Boxers.toString());
+            }
         }
     }
+}
