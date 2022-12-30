ffcf951380b9c6d8448560ee949b9662f0fc8732
07elbekdev <masrapovelbek24@gmail.com>
2022-12-30 15:47:17 +0600

    Hello world.

--- src/animal/BreedOfAnimals.java
+++ src/animal/BreedOfAnimals.java
@@ -1,5 +1,16 @@
 package animal;
 
 public enum BreedOfAnimals {
-    Boxers, Mastiff, German, Shepherds, Border, Collie
+    Boxers("It's Boxer"), Mastiff("It's Mastiff"), German("It's German"), Shepherds("It's Shepherds"), Border("It's Border"), Collie("It's Collie");
+  private String name;
+
+    BreedOfAnimals(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/Service.java
+++ src/main/Service.java
@@ -1,17 +1,16 @@
 package main;
 
+import animal.BreedOfAnimals;
+
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Service {
-    public static void main(String[] args) {
-        method();
-    }
-
-    public static void method() {
-        Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        String b = in.nextLine();
-        String result = a + b;
-        System.out.println(result);
+    void search(){
+        Scanner a = new Scanner(System.in);
+        String b = a.nextLine();
+        if (Objects.equals(b,BreedOfAnimals.Boxers)) {
+            System.out.println(BreedOfAnimals.Boxers.name());
+        }
     }
 }
